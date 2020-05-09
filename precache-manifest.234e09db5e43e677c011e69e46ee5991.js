b3dd92f8abcd377cba31ef987fbca7ae132e1164
nevtay <nevintay@gmail.com>
2020-05-10 00:06:35 +0800

    Updates

---
+++ precache-manifest.234e09db5e43e677c011e69e46ee5991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a346bab3cce212e7f983e62a1cee4e6b",
+    "url": "/react-hooks-and-react-testing-library-practice/index.html"
+  },
+  {
+    "revision": "5fd74eea36dc66fddac6",
+    "url": "/react-hooks-and-react-testing-library-practice/static/css/main.6f7130fc.chunk.css"
+  },
+  {
+    "revision": "5a4942bba9687935bd49",
+    "url": "/react-hooks-and-react-testing-library-practice/static/js/2.ba253b37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-and-react-testing-library-practice/static/js/2.ba253b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd74eea36dc66fddac6",
+    "url": "/react-hooks-and-react-testing-library-practice/static/js/main.620f5faa.chunk.js"
+  },
+  {
+    "revision": "bbc8d963b4dc24d9bfb6",
+    "url": "/react-hooks-and-react-testing-library-practice/static/js/runtime-main.c1f7920a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb4ed9d94a15c64b7c3576812a0335a1.js"
+  "/react-hooks-and-react-testing-library-practice/precache-manifest.234e09db5e43e677c011e69e46ee5991.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hooks-and-react-testing-library-practice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
