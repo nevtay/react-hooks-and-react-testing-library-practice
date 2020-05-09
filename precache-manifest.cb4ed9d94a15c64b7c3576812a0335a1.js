789655ebe2c6a84bceabd2658c11213bff8c89c8
nevtay <nevintay@gmail.com>
2020-05-09 23:56:45 +0800

    Updates

---
+++ precache-manifest.cb4ed9d94a15c64b7c3576812a0335a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e8d4f83e57781df7a3c78ace8bf747",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b3f27f0306509c1a10",
+    "url": "/static/css/main.6f7130fc.chunk.css"
+  },
+  {
+    "revision": "5a4942bba9687935bd49",
+    "url": "/static/js/2.ba253b37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba253b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b3f27f0306509c1a10",
+    "url": "/static/js/main.e771858f.chunk.js"
+  },
+  {
+    "revision": "64f02cd9f16f8872d6a3",
+    "url": "/static/js/runtime-main.cce02f80.js"
+  }
+]);
